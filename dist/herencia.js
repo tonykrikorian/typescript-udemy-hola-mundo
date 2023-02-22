e28d2490070a1d79065f84b8e2e5308041ab73d3
tony krikorian <tony.krikorian@outlook.com>
2023-02-22 15:57:32 -0300

    feat: 🎸 Add herencia, override and access modifiers

---
+++ dist/herencia.js
@@ -0,0 +1,45 @@
+"use strict";
+class DatosBasicos {
+    constructor(name, description, createdAt, createdBy) {
+        this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.createdBy = createdBy;
+    }
+    get fullYear() {
+        return this.createdAt.getFullYear();
+    }
+    get fullDescription() {
+        return `${this.name}: ${this.description}`;
+    }
+}
+class Producto extends DatosBasicos {
+    constructor(stock, sku, price, name, description, createdAt, createdBy) {
+        super(name, description, createdAt, createdBy);
+        this.stock = stock;
+        this.sku = sku;
+        this.price = price;
+    }
+    get fullDescription() {
+        return `producto: ${super.fullDescription}`;
+    }
+}
+class Categoria extends DatosBasicos {
+    constructor(name, description, createdAt, createdBy) {
+        super(name, description, createdAt, createdBy);
+        this.productos = [];
+    }
+    agregarProducto(producto) {
+        this.productos.push(producto);
+    }
+    get fullDescription() {
+        return `categoria: ${this.name}`;
+    }
+}
+const producto1 = new Producto(100, 1, 1000, "iphone", "SmartPhone", new Date(), 1);
+const producto2 = new Producto(100, 2, 2000, "Guitarra Fender Stratocaster", "Guitarra electrica", new Date(), 1);
+const category = new Categoria("celulares", "telefonos inteligentes", new Date(), 1);
+category.agregarProducto(producto1);
+category.agregarProducto(producto2);
+console.log(category.productos, producto1.fullDescription, producto2.fullDescription, category.fullDescription);
+//# sourceMappingURL=herencia.js.map
---
+++ src/herencia.ts
@@ -0,0 +1,112 @@
+/**
+ * Tenemos una clase producto
+ * nombre
+ * descripción
+ * createdAt
+ * createdBy
+ * stock
+ * sku: Codigo de barras
+ */
+
+/**
+ * Clase categoria
+ * nombre
+ * description
+ * createdAt
+ * createdBy
+ */
+
+/**
+ * Debemos crear una clase que agrupe las propiedades comunes
+ */
+
+class DatosBasicos {
+  constructor(
+    public name: string,
+    public description: string,
+    protected createdAt: Date,
+    protected createdBy: number
+  ) {}
+
+  get fullYear(): number {
+    return this.createdAt.getFullYear();
+  }
+
+  get fullDescription(): string {
+    return `${this.name}: ${this.description}`;
+  }
+}
+
+class Producto extends DatosBasicos {
+  constructor(
+    public stock: number,
+    public sku: number,
+    public price: number,
+    name: string,
+    description: string,
+    createdAt: Date,
+    createdBy: number
+  ) {
+    super(name, description, createdAt, createdBy);
+  }
+
+  override get fullDescription() {
+    return `producto: ${super.fullDescription}`;
+  }
+}
+
+class Categoria extends DatosBasicos {
+  public productos: Producto[] = [];
+  constructor(
+    name: string,
+    description: string,
+    createdAt: Date,
+    createdBy: number
+  ) {
+    super(name, description, createdAt, createdBy);
+  }
+
+  public agregarProducto(producto: Producto) {
+    this.productos.push(producto);
+  }
+
+  override get fullDescription() {
+    return `categoria: ${this.name}`;
+  }
+}
+
+const producto1 = new Producto(
+  100,
+  1,
+  1000,
+  "iphone",
+  "SmartPhone",
+  new Date(),
+  1
+);
+const producto2 = new Producto(
+  100,
+  2,
+  2000,
+  "Guitarra Fender Stratocaster",
+  "Guitarra electrica",
+  new Date(),
+  1
+);
+
+const category = new Categoria(
+  "celulares",
+  "telefonos inteligentes",
+  new Date(),
+  1
+);
+
+category.agregarProducto(producto1);
+category.agregarProducto(producto2);
+
+console.log(
+  category.productos,
+  producto1.fullDescription,
+  producto2.fullDescription,
+  category.fullDescription
+);
